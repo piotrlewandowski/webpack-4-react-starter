20be2d5f12693ff67d4a54ce0f4405f693d03146
Piotr Lewandowski <contact@piotrlewandowski.info>
2020-09-11 12:48:18 +0100

    ErrorBoundary component refactoring; Add eslint parser

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 
 export class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
+  state = { hasError: false, error: '' };
 
-  static getDerivedStateFromError() {
+  static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { hasError: true, error: error.toString() };
   }
 
-  componentDidCatch() {
+  componentDidCatch(error, info) {
     // You can also log the error to an error reporting service
+    console.error(error, info);
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return (
+        <div>
+          <h1>App crashed</h1>
+          <p>Something went wrong.</p>
+          {this.state.error}
+        </div>
+      );
     }
 
     return this.props.children;
